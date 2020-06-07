d37c405edbf26a077b1d52898728d606825db3e5
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-06-07 15:10:00 -0300

    feat: navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,30 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Fragment } from "react";
+import { AppLoading } from "expo";
+import { StatusBar } from "react-native";
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
+
+import Routes from "./src/routes";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
+    <Fragment>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </Fragment>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#7159c1",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
