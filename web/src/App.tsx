923b648cf0a46c77118f6da1c45df1cc171bb792
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-06-07 10:18:32 -0300

    feat: routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import Home from "./pages/Home/index";
+import Routes from "./routes";
 
 function App() {
-  return <Home />;
+  return <Routes />;
 }
 
 export default App;
